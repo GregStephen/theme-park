910a13b2fc9890bdd067ecf7b39f3b1345ac22f9
Greg Stephen <GregAStephen@gmail.com>
2019-04-02 21:32:47 -0500

    WIP

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log("Yo dude");
+// XHR - XmlHttpRequest
+let rides = [];
+
+const domStringBuilder = (arrayToPrint) => {
+    console.log("arrayToPrint", arrayToPrint);
+}
+
+function executeThisCodeAfterFileLoads(){
+    const data = JSON.parse(this.responseText);
+    rides = data.rides;
+    domStringBuilder(data.rides);
+};
+
+function executeThisCodeIfXHRFails(){
+    console.error('oh shit');
+};
+
+const getRidesData = () => {
+    const myRequest = new XMLHttpRequest();
+    myRequest.addEventListener('load', executeThisCodeAfterFileLoads);
+    myRequest.addEventListener('error', executeThisCodeIfXHRFails);
+    myRequest.open('GET', './db/rides.json');
+    myRequest.send();
+};
+
+
+const init = () => {
+    getRidesData();
+};
+
+init();
